92e01f81c7c37b805c0570552f50734880ca769f
marcosblandim <marcosblandim@gmail.com>
2022-02-15 20:09:32 -0300

    test

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -2,9 +2,8 @@ import ButtonMui from '@mui/material/Button';
 import styled from 'styled-components';
 
 type Props = {
-  marginTop?: string;
-}
+  marginTop?: string;}
 
 export const Button = styled(ButtonMui)<Props>`
-  margin-top: ${props => props.marginTop || '0px'};
+  margin-top: ${props => props.marginTop || '0px'}
 `;
